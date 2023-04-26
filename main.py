3719dddef806317d2b862013bbaa489f9eef7c64
Pratham sharma <vaderd233@gmail.com>
2023-04-27 04:05:46 +0530

    added footer and main page content

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import streamlit as st
 
+footer = """
+<style>
+footer:after{
+    content:'Copyright ©️ Pratham Sharma 2023';
+    display:block;
+    padding:5px
+    top:3px;
+}
+</style>
+"""
+
 
 def app():
     st.set_page_config(
         page_title='Cyber Sec',
         page_icon='👾',
     )
+    st.markdown(footer, unsafe_allow_html=True)
     st.header("Welcome To The Ciphers Calculator App")
-    st.caption("Copyright ©️ Pratham Sharma 2023")
+    st.subheader("⬅️ Choose a Cipher from the side menu")
 
 
 if __name__ == '__main__':
