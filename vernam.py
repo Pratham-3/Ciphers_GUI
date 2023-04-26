8756e091102e56fd7dec419b2f3998eec335e277
VictoriousMango <additya.mishra20@st.niituniversity.in>
2023-04-27 02:12:31 +0530

    Webpage of Vernam Cipher is Ready

--- vernam.py
+++ vernam.py
@@ -1,4 +1,5 @@
 import random
+import streamlit as st
 
 
 # Function to generate a random key of specified length
@@ -25,21 +26,23 @@ def vernam_cipher_decrypt(cipher_text, key):
     return plain_text
 
 
-# Example usage
-print("Enter Plain Text: ")
-plain_text = input()
-key = generate_key(len(plain_text))
-cipher_text = vernam_cipher_encrypt(plain_text, key)
-decrypted_text = vernam_cipher_decrypt(cipher_text, key)
-
-print("Plaintext:", plain_text)
-print("Key:", key)
-print("Ciphertext:", cipher_text)
-print("Decrypted Text:", decrypted_text)
-
-
 def app():
-    pass
+    st.title('Vernam Cipher')
+    plain_text = st.text_input("Enter Plain Text: ")
+    plain_text = plain_text.upper()
+    if plain_text:
+        key = generate_key(len(plain_text))
+        cipher_text = vernam_cipher_encrypt(plain_text, key)
+        decrypted_text = vernam_cipher_decrypt(cipher_text, key)
+        col1, col2 = st.columns([1, 1])
+        col1.write(f"Generated Key: {key}")
+        text = f"Ciphertext: {cipher_text}"
+        col1.success(text)
+        col2.write(f"Plaintext: {plain_text}")
+        text = f"Decrypted Text: {decrypted_text}"
+        col2.success(text)
+
+
 
 
 if __name__ == '__main__':
